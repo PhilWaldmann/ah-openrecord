7907c563e145b4e47551fafcc3cb4408de05d34b
Philipp Waldmann <philipp.waldmann@s-team.at>
2015-01-27 15:24:00 +0100

    add api + logger to api.db

--- initializers/database.js
+++ initializers/database.js
@@ -3,7 +3,10 @@ var OpenRecord = require('openrecord');
 exports.database = function(api, next){
 
   api.config.database.logger = api.logger;  
-  api.logger.trace = function(){};
+  api.logger.trace = function(){};  
+  api.logger.warn = api.logger.warning;
+    
+  api.config.database.api = api;
     
   api.db = new OpenRecord(api.config.database);
   api.db.ready(next);
